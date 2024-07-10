53e0ac560e487ad339013ffe08fb245c185b11fb
SamkovLuka <staddycase@gmail.com>
2024-07-10 23:32:37 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,85 @@
+
+
+#1
+
+#не зроблено
+
+
+# list = [2,5,4,3,7]
+# def listofmult(list):
+#     for i in list:
+#         print(i*i)
+
+
+# listofmult(list)
+
+#2
+
+#зроблено
+
+# list1 = [2,5,4,3,7]
+# def listofmin(list1):
+#     print(min(list1))
+
+# print(listofmin(list1))
+
+#3
+
+#зроблено
+
+# list2 = [2,5,4,3,7]
+# def listofsimple(list2):
+#     n = 0
+#     for i in list2:
+#         if i%2 == 0:
+#             print(".")
+#         elif i%i == 0:
+#             n+=1
+#         elif i%1 == 0:
+#             n+=1
+#     print(f"кількість простих чисел - {n}")
+
+# print(listofsimple(list2))
+
+
+#4
+
+#зроблено
+
+# list3 = [2,5,4,3,5,3,5,7,5,5]
+# def listofnum(list3):
+#     k = list3.count(5)
+#     list3.remove(5)
+#     print(f"кількість видалених елементів - {k}")
+
+# print(listofnum(list3))
+
+
+#5
+#зроблено
+
+# list_f = [3,45,7,19,93,5]
+# list_n = [90,45,23,7,19,71]
+# def printbothlist(list_f,list_n):
+#     list_m = list_f+list_n
+#     for i in list_m:
+#       if list_m.count(i) == 1:
+#          list_m.remove(i)
+#     print(list_m)
+
+# print(printbothlist(list_f,list_n))
+
+#6
+
+#зроблено
+
+# gmd_list = [2,8,324,56,3]
+# h = 2
+# new_list = []
+# def printPow(gmd_list, h):
+#    for i in gmd_list:
+#       new_list.append(i**h)
+#    print(new_list)
+
+
+# printPow(gmd_list, h)
